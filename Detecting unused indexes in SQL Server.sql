dca2623f855e8bc1d12ca79133599eb6a383208d
Birol Topuz <birolnet@gmail.com>
2023-04-06 13:17:09 +0300

    Update Detecting unused indexes in SQL Server.sql

--- Detecting unused indexes in SQL Server.sql
+++ Detecting unused indexes in SQL Server.sql
@@ -11,4 +11,6 @@ INNER JOIN (
 ) p ON p.index_id = dm_ius.index_id AND dm_ius.object_id = p.object_id
 WHERE OBJECTPROPERTY(dm_ius.object_id, 'IsUserTable') = 1 AND dm_ius.database_id = DB_ID()
     AND i.type_desc = 'nonclustered' AND i.is_primary_key = 0 AND i.is_unique_constraint = 0
+	--AND dm_ius.database_id = db_id('YourDatabaseName')
+    --AND dm_ius.object_id = object_id('YourTableName')
 ORDER BY (dm_ius.user_seeks + dm_ius.user_scans + dm_ius.user_lookups) ASC
