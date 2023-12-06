e02a1e6e9f303cdea232fa8502f548c873beb31c
Birol Topuz <birolnet@gmail.com>
2023-12-07 00:11:32 +0300

    Update Get Status of Running Backup and Restore.sql

--- SQLServer/Get Status of Running Backup and Restore.sql
+++ SQLServer/Get Status of Running Backup and Restore.sql
@@ -1,21 +1,14 @@
-SELECT r.session_id AS [Session_Id]
-	,r.command AS [command]
-	,CONVERT(NUMERIC(6, 2), r.percent_complete) AS [% Complete]
-	,GETDATE() AS [Current Time]
-	,CONVERT(VARCHAR(20), DATEADD(ms, r.estimated_completion_time, GetDate()), 20) AS [Estimated Completion Time]
-	,CONVERT(NUMERIC(32, 2), r.total_elapsed_time / 1000.0 / 60.0) AS [Elapsed Min]
-	,CONVERT(NUMERIC(32, 2), r.estimated_completion_time / 1000.0 / 60.0) AS [Estimated Min]
-	,CONVERT(NUMERIC(32, 2), r.estimated_completion_time / 1000.0 / 60.0 / 60.0) AS [Estimated Hours]
-	,CONVERT(VARCHAR(1000), (
-			SELECT SUBSTRING(TEXT, r.statement_start_offset / 2, CASE 
-						WHEN r.statement_end_offset = - 1
-							THEN 1000
-						ELSE (r.statement_end_offset - r.statement_start_offset) / 2
-						END) 'Statement text'
-			FROM sys.dm_exec_sql_text(sql_handle)
-			))
+SELECT r.session_id AS [Session_Id], r.command AS [Command], CONVERT(NUMERIC(6, 2), r.percent_complete) AS [% Complete]
+, GETDATE() AS [Current Time], CONVERT(VARCHAR(20), DATEADD(ms, r.estimated_completion_time, GetDate()), 20) AS [Estimated Completion Time]
+, CONVERT(NUMERIC(32, 2), r.total_elapsed_time / 1000.0 / 60.0) AS [Elapsed Min]
+, CONVERT(NUMERIC(32, 2), r.estimated_completion_time / 1000.0 / 60.0) AS [Estimated Min]
+, CONVERT(NUMERIC(32, 2), r.estimated_completion_time / 1000.0 / 60.0 / 60.0) AS [Estimated Hours]
+, CONVERT(VARCHAR(1000), (
+    SELECT SUBSTRING(TEXT, r.statement_start_offset / 2, (CASE WHEN r.statement_end_offset = - 1 THEN 1000
+        ELSE (r.statement_end_offset - r.statement_start_offset) / 2 END))
+    FROM sys.dm_exec_sql_text(sql_handle)
+)) AS  [Statement Text]
 FROM sys.dm_exec_requests r
-WHERE command like 'RESTORE%'
-or	command like 'BACKUP%'
+WHERE command like 'RESTORE%' or command like 'BACKUP%' or command like 'DBCC%'
 
 -- Source => https://blog.sqlauthority.com/2017/03/12/get-status-running-backup-restore-sql-server-interview-question-week-113/
